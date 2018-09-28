15f46087da615aaf556311201daa66c1a9f8126c
SpQuyt <spquyt@gmail.com>
2018-09-28 16:08:13 +0700

    done creating and made qr code scanner running

---
+++ App.js
@@ -0,0 +1,55 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Linking } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+// import { createStackNavigator } from 'react-navigation';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={{ flex: 1 }}
+          barCodeTypes={[RNCamera.Constants.BarCodeType.qr]}
+          onBarCodeRead={(data) => {
+            Linking
+              .openURL(data.data)
+          }
+
+          }
+        >
+
+        </RNCamera>
+        <Text>Go PRO</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+    // backgroundColor: 'transparent'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
