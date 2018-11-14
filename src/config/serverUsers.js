187bf9ae5c6436ca04066af2e9b880c09a5c4ba1
Trương Anh Quốc <quocta@talent.ominext.com>
2018-11-14 23:45:09 +0700

    finally an api server

--- src/config/serverUsers.js
+++ src/config/serverUsers.js
@@ -1,5 +1,5 @@
 
-const ServerUsers = 'http://192.168.1.5:3000/users';
+const ServerUsers = 'https://api.namdaoduy.tk/easy-event/users';
 // const ServerUsers = 'http://192.168.0.103:3000/users';
 
 module.exports = ServerUsers;
